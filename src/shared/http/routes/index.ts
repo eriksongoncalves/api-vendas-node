b2f25c6b55f5cde5cce600bbaa81b48323159bfb
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-09 15:00:27 -0300

    feat: add controller and forgot route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
 
 const router = Router();
 
 router.use('/products', productsRouter);
 router.use('/users', usersRouter);
 router.use('/sessions', sessionsRouter);
+router.use('/password', passwordRouter);
 
 export default router;
