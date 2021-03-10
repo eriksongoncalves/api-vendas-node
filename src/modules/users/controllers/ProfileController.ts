e5a7b68f736ec82ccd02ee4a00d567a3b921e47a
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-10 11:12:21 -0300

    feat: profile controller

---
+++ src/modules/users/controllers/ProfileController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+
+import ShowProfileService from '../services/ShowProfileService';
+import UpdateProfileService from '../services/UpdateProfileService';
+
+class ProfileController {
+  async show(req: Request, res: Response) {
+    const service = new ShowProfileService();
+    const user_id = req.user.id;
+
+    const user = await service.execute({ user_id });
+
+    return res.json(user);
+  }
+
+  async create(req: Request, res: Response) {
+    const { name, email, password, old_password } = req.body;
+    const user_id = req.user.id;
+
+    const service = new UpdateProfileService();
+
+    const user = await service.execute({
+      user_id,
+      name,
+      email,
+      password,
+      old_password
+    });
+
+    return res.json(user);
+  }
+}
+
+export default ProfileController;
