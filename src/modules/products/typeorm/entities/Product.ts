172291da0bf118093ca82aa3a96cf73b5d70c997
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-10 13:22:30 -0300

    feat: entity order and orders products

--- src/modules/products/typeorm/entities/Product.ts
+++ src/modules/products/typeorm/entities/Product.ts
@@ -3,13 +3,19 @@ import {
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
-  UpdateDateColumn
+  UpdateDateColumn,
+  OneToMany
 } from 'typeorm';
+
+import OrdersProducts from '@modules/orders/typeorm/entities/OrdersProducts';
 
 @Entity('products')
 class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @OneToMany(() => OrdersProducts, order_products => order_products.product)
+  order_products: OrdersProducts[];
 
   @Column()
   name: string;
