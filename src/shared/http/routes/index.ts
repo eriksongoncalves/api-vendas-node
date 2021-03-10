bfceb3d032cc9c352cfea4e5c48ec6849aa64e9b
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-10 12:10:19 -0300

    feat: customer routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -5,6 +5,7 @@ import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
+import customersRouter from '@modules/customers/routes/customer.routes';
 
 const router = Router();
 
@@ -13,5 +14,6 @@ router.use('/users', usersRouter);
 router.use('/sessions', sessionsRouter);
 router.use('/password', passwordRouter);
 router.use('/profile', profileRouter);
+router.use('/customers', customersRouter);
 
 export default router;
