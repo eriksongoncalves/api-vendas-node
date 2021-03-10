a55f20e1eee5a0b6cf53ac804a3d8b5905fee199
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-03-10 15:20:33 -0300

    feat: add pagination on list customer service

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,12 +1,26 @@
 import { getCustomRepository } from 'typeorm';
 
+import Customer from '../typeorm/entities/Customer';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
+
 class ListCustomerService {
-  async execute() {
+  async execute(): Promise<IPaginateCustomer> {
     const repository = getCustomRepository(CustomersRepository);
 
-    return repository.find();
+    const customers = await repository.createQueryBuilder().paginate();
+
+    return customers as IPaginateCustomer;
   }
 }
 
